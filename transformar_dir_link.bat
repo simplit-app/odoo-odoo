82daa2e6961fa22065d8c94300c865d9634a0e63
miguel-molinari <miguel.molinari@gmail.com>
2024-04-26 09:29:56 -0300

    Create transformar_dir_link.bat

---
+++ transformar_dir_link.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "directory=.\addons_simplit"
+
+for %%f in ("%directory%\*") do (
+    set "filename=%%f"
+    set "content="
+
+    for /f "tokens=* delims=" %%a in (!filename!) do (
+        set "content=!content!%%a"
+    )
+
+    rem Eliminar cualquier enlace simbólico previo con el mismo nombre
+    del "%directory%\%%~nf"
+
+    rem Crear un enlace simbólico con el nombre del archivo y el contenido como destino
+    mklink "%directory%\%%~nf" "!content!"
+	
+)
+
+endlocal
